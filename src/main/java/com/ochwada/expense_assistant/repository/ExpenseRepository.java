c4e7c8114b494bfebfadf9f7df05bfa355fa70fe
ochwada <nakhulolinda@yahoo.com>
2025-07-18 16:52:04 +0200

    added repository infrace

---
+++ src/main/java/com/ochwada/expense_assistant/repository/ExpenseRepository.java
@@ -0,0 +1,17 @@
+package com.ochwada.expense_assistant.repository;
+
+
+/**
+ * *******************************************************
+ *  Package: com.ochwada.expense_assistant.repository
+ *  File: ExpenseRepository.java
+ *  Author: Ochwada
+ *  Date: Friday, 18.Jul.2025, 4:50 PM
+ *  Description: Repository interface for the conversion document. Spring Data MongoDB automatically implements the  * interface at runtime, providing built-in CRUD methods
+ *  Objective: 
+ * *******************************************************
+ */
+ 
+ 
+ public class ExpenseRepository {
+}
---
+++ src/main/java/com/ochwada/expense_assistant/service/ExpenseService.java
@@ -0,0 +1,17 @@
+package com.ochwada.expense_assistant.service;
+
+
+/**
+ * *******************************************************
+ *  Package: com.ochwada.expense_assistant.service
+ *  File: ExpenseService.java
+ *  Author: Ochwada
+ *  Date: Friday, 18.Jul.2025, 4:48 PM
+ *  Description: 
+ *  Objective: 
+ * *******************************************************
+ */
+ 
+ 
+ public class ExpenseService {
+}
